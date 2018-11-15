359ddb7382ed7451cffef0ff21bc94a04fd6c1e4
Paul <paulesammut@gmail.com>
2018-11-15 15:19:31 +1100

    Formatting only

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ export default class App extends Component {
     render() {
         return <div className='list'>
             <Sortable dragClassName='drag-style'>
-            {items.map(item =>
-                <div key={item} className='list-item'>{item}</div>)
-            }
+                {items.map(item =>
+                    <div key={item} className='list-item'>{item}</div>)
+                }
             </Sortable>
         </div>
     }
