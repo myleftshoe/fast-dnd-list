38ae6c7f6c0db7949f462910c309c65ad3573d66
Paul <paulesammut@gmail.com>
2018-11-28 23:46:57 +1100

    made arrow functions into proper functions in events.js

--- src/events.js
+++ src/events.js
@@ -1,9 +1,12 @@
-export const preventDefault = e => e.preventDefault();
+export function preventDefault(e) { e.preventDefault() }
+export function stopPropagation(e) { e.stopPropagation() }
 
-export const fireAndForget = (target, eventName) => new Promise((resolve, reject) => {
-    const handler = ({ target }) => {
-        target.removeEventListener(eventName, handler, false);
-        resolve();
-    }
-    target.addEventListener(eventName, handler, false);
-})
+export function fireAndForget(target, eventName) {
+    return new Promise((resolve, reject) => {
+        const handler = ({ target }) => {
+            target.removeEventListener(eventName, handler, false);
+            resolve();
+        }
+        target.addEventListener(eventName, handler, false);
+    })
+}
