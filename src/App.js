72da2d60b93e35091c6f23a0eb2e37fbfa05d143
Paul <paulesammut@gmail.com>
2018-11-28 12:08:29 +1100

    Refactored App as funtional component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Sortable from './Sortable';
-import { move, shuffle, reverse } from './array';
+import { move } from './array';
 
 const generateItems = length => [...Array(length).keys()].map(k => `Item ${k}`)
 
-const actions = {
-    reverse: ({ items }) => ({ items: reverse(items) }),
-    shuffle: ({ items }) => ({ items: shuffle(items) }),
-    move: (oldIndex, newIndex) => ({ items }) => ({ items: move(items, oldIndex, newIndex) }),
-}
+export default function App() {
 
-export default class App extends Component {
+    let [items, setItems] = useState(generateItems(80));
 
-    state = { items: generateItems(80) }
+    const onDrop = ({ oldIndex, newIndex }) => setItems(move(items, oldIndex, newIndex));
 
-    onDrop = ({ oldIndex, newIndex }) => this.setState(actions.move(oldIndex, newIndex))
-
-    render() {
-        return <div className='list'>
-            <Sortable dragClassName='drag-style' onDrop={this.onDrop}>
-                {this.state.items.map(item =>
-                    <div key={item} className='list-item'>{item}</div>)
-                }
-            </Sortable>
-        </div>
-    }
+    return <div className='list'>
+        <Sortable dragClassName='drag-style' onDrop={onDrop}>
+            {items.map(item =>
+                <div key={item} className='list-item'>{item}</div>)
+            }
+        </Sortable>
+    </div>
 }
