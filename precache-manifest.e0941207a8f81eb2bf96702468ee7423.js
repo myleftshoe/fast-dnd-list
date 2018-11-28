c4500f825d5dd2d00968c2416f64da276d726214
Paul <paulesammut@gmail.com>
2018-11-28 13:59:28 +1100

    Updates

--- precache-manifest.e0941207a8f81eb2bf96702468ee7423.js
+++ precache-manifest.e0941207a8f81eb2bf96702468ee7423.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/fast-dnd-list/static/js/runtime~main.06a6e9b2.js"
   },
   {
-    "revision": "55fefef7b83bab7feb0e",
-    "url": "/fast-dnd-list/static/js/main.55fefef7.chunk.js"
+    "revision": "bdacc5304bcb389fb455",
+    "url": "/fast-dnd-list/static/js/main.bdacc530.chunk.js"
   },
   {
     "revision": "5826b35959d19850e242",
     "url": "/fast-dnd-list/static/js/1.5826b359.chunk.js"
   },
   {
-    "revision": "55fefef7b83bab7feb0e",
-    "url": "/fast-dnd-list/static/css/main.ed2ee2f3.chunk.css"
+    "revision": "bdacc5304bcb389fb455",
+    "url": "/fast-dnd-list/static/css/main.d3945abd.chunk.css"
   },
   {
-    "revision": "b4b45824fcc705a2e7e7cb41f5bdc609",
+    "revision": "819f3488a7d942eb2c72be58aeea39c2",
     "url": "/fast-dnd-list/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fast-dnd-list/precache-manifest.52d027cdc8da8b535192a2cf3a5987bc.js"
+  "/fast-dnd-list/precache-manifest.e0941207a8f81eb2bf96702468ee7423.js"
 );
 
 workbox.clientsClaim();
