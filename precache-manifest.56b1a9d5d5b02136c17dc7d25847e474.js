d14f73db2098896d2fadf0306c87ad3f06db45ec
Paul <paulesammut@gmail.com>
2018-11-29 15:07:10 +1100

    Updates

--- precache-manifest.56b1a9d5d5b02136c17dc7d25847e474.js
+++ precache-manifest.56b1a9d5d5b02136c17dc7d25847e474.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/fast-dnd-list/static/js/runtime~main.06a6e9b2.js"
   },
   {
-    "revision": "d3dda7c69aab5963a99f",
-    "url": "/fast-dnd-list/static/js/main.d3dda7c6.chunk.js"
+    "revision": "0c0248a7262cbb549218",
+    "url": "/fast-dnd-list/static/js/main.0c0248a7.chunk.js"
   },
   {
     "revision": "3c133105251be7e2c3ff",
     "url": "/fast-dnd-list/static/js/1.3c133105.chunk.js"
   },
   {
-    "revision": "d3dda7c69aab5963a99f",
-    "url": "/fast-dnd-list/static/css/main.66c80de5.chunk.css"
+    "revision": "0c0248a7262cbb549218",
+    "url": "/fast-dnd-list/static/css/main.5b836dec.chunk.css"
   },
   {
-    "revision": "6a4ad773fb25e69ac540a47129d00b7f",
+    "revision": "10b41ec4d2802d2b921516029cba4905",
     "url": "/fast-dnd-list/index.html"
   }
 ];
