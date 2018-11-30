2353329a30d3ab12d88f865acf8b44c81d4ddf66
Paul <paulesammut@gmail.com>
2018-11-30 22:38:51 +1100

    separate mouse and pointer events

--- src/Sortable.js
+++ src/Sortable.js
@@ -8,46 +8,93 @@ import { preventDefault } from './events';
 export default function Sortable(props) {
 
     let [handler, setHandler] = useState();
-    let [started, setStarted] = useState();
-
+    let [started, setStarted] = useState(false);
+    let [usingTouch, setUsingTouch] = useState(false);
     useEffect(() => {
         setHandler(new Handler(containerRef.current, props));
     }, [props.children]);
 
     // workaround to prevent mousedown event firing after 300ms when using touch
-    useEffect(() => { containerRef.current.ontouchstart = preventDefault })
+    useEffect(() => {
+        // if (preventMouse)
+        //     containerRef.current.ontouchstart = preventDefault;
+        // else
+        //     containerRef.current.ontouchstart = null;
+        // console.log(containerRef.current.ontouchstart);
+    })
 
     const containerRef = useRef();
 
-    function start(e) {
+    function onPointerDown(e) {
+        console.log('onPointerDown');
+        if (e.pointerType === 'touch')
+            setUsingTouch(true);
+        else
+            setUsingTouch(false);
+    }
+
+    function touchStart(e) {
+        console.log('touchStart');
+        // setUsingTouch(true);
         if (e.target === containerRef.current) return;
-        setStarted(true);
         handler.grasp(e.target);
         props.onGrasp && props.onGrasp();
     }
 
-    function move(e) {
-        handler.move(e.clientX, e.clientY);
+    function touchMove(e) {
+        console.log('touchMove');
+        handler.move(e.touches[0].clientX, e.touches[0].clientY);
         props.Drag && props.onDrag();
     }
 
-    function end() {
+    function touchEnd() {
+        console.log('touchEnd');
         const result = handler.release();
         setStarted();
         props.onDrop && props.onDrop(result);
     }
 
-    const handleStart = handler && start;
-    const handleMove = started && handler && move;
-    const handleEnd = handler && end;
+    function mouseDown(e) {
+        console.log('mouseDown');
+        setStarted(true);
+        // setUsingTouch(false);
+        if (e.target === containerRef.current) return;
+        handler.grasp(e.target);
+        props.onGrasp && props.onGrasp();
+    }
+
+    function mouseMove(e) {
+        console.log('mouseMove');
+        handler.move(e.clientX, e.clientY);
+        props.Drag && props.onDrag();
+    }
+
+    function mouseUp() {
+        console.log('mouseUp');
+        const result = handler.release();
+        setStarted(false);
+        props.onDrop && props.onDrop(result);
+    }
+
+    const handleTouchStart = usingTouch && handler ? touchStart : null;
+    const handleTouchMove = usingTouch && handler ? touchMove : null;
+    const handleTouchEnd = usingTouch && handler ? touchEnd : null;
+
+    const handleMouseDown = !usingTouch && handler ? mouseDown : null;
+    const handleMouseMove = started && !usingTouch && handler ? mouseMove : null;
+    const handleMouseUp = !usingTouch && handler ? mouseUp : null;
 
     return <div style={{ overflowY: 'scroll' }}>
         <div
             className='container'
             ref={containerRef}
-            onPointerDown={handleStart}
-            onPointerMove={handleMove}
-            onPointerUp={handleEnd}
+            onPointerDown={onPointerDown}
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEnd}
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}
             onContextMenu={preventDefault}
         >
             {props.children}
