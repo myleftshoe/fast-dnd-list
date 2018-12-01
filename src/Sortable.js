fce5ad9834f310a49267faf667e563da549eb933
Paul <paulesammut@gmail.com>
2018-12-01 19:14:46 +1100

    Add mousemove event listener on doc body to cater for mouse being dragged outside container

--- src/Sortable.js
+++ src/Sortable.js
@@ -17,7 +17,17 @@ export default function Sortable(props) {
         setHandler(new Handler(containerRef.current, props));
     }, [props.children]);
 
-    function onPointerDown(e) {
+    useEffect(() => {
+        if (!usingTouch && started)
+            document.body.addEventListener("mousemove", move);
+        else
+        document.body.removeEventListener("mousemove", move);
+        return () => {
+            document.body.removeEventListener("mousemove", move);
+        };
+      });
+
+    function handlePointerDown(e) {
         if (e.pointerType === 'touch')
             setUsingTouch(true);
         else
@@ -56,22 +66,44 @@ export default function Sortable(props) {
         }
         else events = {
             onMouseDown: start,
-            onMouseMove: started ? move : null,
+            // onMouseMove: started ? move : null,
             onMouseUp: end,
+            // onMouseLeave:  end,
         }
     }
 
-    return <div style={{ overflowY: 'scroll' }}>
-        <div
-            className='container'
-            ref={containerRef}
-            onPointerDown={onPointerDown}
+    return (
+        //
+        // Notes:
+        //
+        // 1. The draggable is positioned relatively. In order for it to not be cut-off when dragged
+        // beyond the container edges requires overflow-x: visible on the container. But it's not
+        // possible to have overflow visible on one axis and allow the other axis to scroll: the
+        // visible axis will be overidden with overflow: auto (google it!).
+        // The solution is to wrap the container with overflow-x: visible in a scrollable div as
+        // below.
+        // Positioning the draggable absolutely allows it to remain visible no matter where it is
+        // but will close the placeholder when grasped. It also requires its size to be tweaked
+        // on grasp as it loses its container padding.
+        //
+        // 2. The event listeners are attached to the wrapper (not the container) as the mousemove
+        // events do not fire when the draggable is outside the container (and dragged fast so that
+        // the mouse leaves the draggable). I.e. the draggable freezes until the mouse is moved over
+        // it again. The container fits the list contenet snuggly whereas the wrapper is the full width
+        // of the page -> events on the wrapper continue to fire.
+        // Update: Now adds mousemove listener to the document so others can go either on container or
+        // wrapper. Leave on wrapper for now.
+
+        <div style={{ overflowY: 'scroll' }}
+            onPointerDown={handlePointerDown}
             {...events}
             onContextMenu={preventDefault}
         >
-            {props.children}
-        </div>
+            <div className='container' ref={containerRef}>
+                {props.children}
+            </div>
     </div >
+    )
 }
 
 //------------------------------------------------------------------------------
