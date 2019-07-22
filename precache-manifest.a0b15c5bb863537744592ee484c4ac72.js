9debffacfeb14e0917ad718a657b16fd6bfc890e
Paul Sammut <paul@pop-os.localdomain>
2019-07-22 12:27:29 +1000

    Updates

---
+++ precache-manifest.a0b15c5bb863537744592ee484c4ac72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b6eac4a282e506a1693f4efdf820a7f",
+    "url": "/fast-dnd-list/index.html"
+  },
+  {
+    "revision": "d7f94443c56505d983da",
+    "url": "/fast-dnd-list/static/css/main.1d6d64e1.chunk.css"
+  },
+  {
+    "revision": "a429891a7a2b71c7ce9b",
+    "url": "/fast-dnd-list/static/js/2.05431fa7.chunk.js"
+  },
+  {
+    "revision": "d7f94443c56505d983da",
+    "url": "/fast-dnd-list/static/js/main.c331c4e3.chunk.js"
+  },
+  {
+    "revision": "d3e01253f24ee62d13ea",
+    "url": "/fast-dnd-list/static/js/runtime~main.66a34e2e.js"
+  }
+]);
